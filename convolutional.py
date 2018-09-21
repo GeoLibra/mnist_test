e48b95dd49b48976767c4056819705acaa4a6f71
LuvJia <674530915@qq.com>
2018-09-21 19:56:44 +0800

    fix: add convolutional model

---
+++ convolutional.py
@@ -0,0 +1,59 @@
+import os
+import module
+import tensorflow as tf
+import input_data
+
+data = input_data.read_data_sets('MNIST_data', one_hot=True)
+# 定义模型
+with tf.variable_scope("convolutional"):
+    x = tf.placeholder(tf.float32, [None, 784], name="x")
+    keep_prob = tf.placeholder(tf.float32)
+    y, variables = module.convolutional(x, keep_prob)
+
+# 训练
+y1 = tf.placeholder(dtype=tf.float32, shape=[None, 10], name='y1')
+cross_entropy = -tf.reduce_sum(y1 * tf.log(y))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y1, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+saver = tf.train.Saver(variables)
+
+with tf.Session() as sess:
+    merged_summary_op = tf.summary.merge_all()
+    summary_writer = tf.summary.FileWriter('mnist_log/1', sess.graph)
+    summary_writer.add_graph(sess.graph)
+
+    sess.run(tf.global_variables_initializer())
+
+    for i in range(20000):
+        batch = data.train.next_batch(50)
+        if i % 100 == 0:
+            train_accuracy = accuracy.eval(feed_dict={
+                x: batch[0],
+                y1: batch[1],
+                keep_prob: 1.0
+            })
+            print("setp %d, train accuracy=%d" % (i, train_accuracy))
+        sess.run(
+            train_step, feed_dict={
+                x: batch[0],
+                y1: batch[1],
+                keep_prob: 0.5
+            })
+        print(
+            sess.run(
+                accuracy,
+                feed_dict={
+                    x: data.test.images,
+                    y1: data.test.labels,
+                    keep_prob: 1.0
+                }))
+        path = saver.save(
+            sess,
+            os.path.join(
+                os.path.dirname(__file__), 'data', 'convalution.ckpt'),
+            write_meta_graph=False,
+            write_state=False)
+
+        print("Saved:", path)
--- module.py
+++ module.py
@@ -8,3 +8,45 @@ def regression(x):
     y = tf.nn.softmax(tf.matmul(x, a) + b)
 
     return y, [a, b]
+
+
+# 定义卷积模型
+def convolutional(x, keep_prob):
+    def conv2d(x, w):
+        return tf.nn.conv2d(
+            x, w, [1, 1, 1, 1],padding='SAME')
+
+    def max_pool_2x2(x):
+        return tf.nn.max_pool(
+            x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    def weight_variable(shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
+
+    def bias_variable(shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
+    w_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+    h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    w_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    # 全连接层
+    w_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    w_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+    y = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2))
+
+    return y, [w_conv1, b_conv1, w_conv2, b_conv2, w_fc1, w_fc2, b_fc2]
