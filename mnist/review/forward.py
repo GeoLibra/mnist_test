5bd64f4126e950110fb104e7485f3f19925ead3f
LuvJia <674530915@qq.com>
2018-10-29 10:04:12 +0800

    feat: add train

--- mnist/review/forward.py
+++ mnist/review/forward.py
@@ -10,7 +10,7 @@ LAYER1_NODE = 500
 '''
 
 
-def get_weight_variable(shpae, regularizer):
+def get_weight_variable(shape, regularizer):
     weights = tf.get_variable(
         "weights",
         shape,
@@ -35,12 +35,12 @@ def forward(input_tensor, regularizer):
         weights = get_weight_variable([INPUT_NODE, LAYER1_NODE], regularizer)
         biases = tf.get_variable(
             "biases", [LAYER1_NODE], initializer=tf.constant_initializer(0.0))
-        layer1tf.nn.relu(tf.matmul(input_tensor, weightsd) + biases)
+        layer1=tf.nn.relu(tf.matmul(input_tensor, weights) + biases)
     # 类似的声明第二层神经网络的变量并完成前向传播过程
-    with tf.variable('layer2'):
+    with tf.variable_scope('layer2'):
         weights = get_weight_variable([LAYER1_NODE, OUTPUT_NODE], regularizer)
         biases = tf.get_variable(
             'biases', [OUTPUT_NODE], initializer=tf.constant_initializer(0.0))
-        layer2 = tf.matmul(layers, weights) + biases
+        layer2 = tf.matmul(layer1, weights) + biases
     # 返回最后前向传播结果
     return layer2
